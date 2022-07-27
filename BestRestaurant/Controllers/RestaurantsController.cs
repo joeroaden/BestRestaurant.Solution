e330b72376e6f5b17f1de9c95ab62263b1a82758
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-07-27 11:49:56 -0700

    update Restaurant Controller and Index Views

---
+++ BestRestaurant/Controllers/RestaurantsController.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurant.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BestRestaurant.Controllers
+{
+  public class RestaurantsController : Controller
+  {
+    private readonly BestRestaurantContext _db;
+
+    public RestaurantsController(BestRestaurantContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Restaurant> model = _db.Restaurants.ToList();
+      ViewBag.PageTitle = "View All Restaurants";
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.PageTitle = "New Restaurant";
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Restaurant restaurant)
+    {
+      _db.Restaurants.Add(restaurant);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
